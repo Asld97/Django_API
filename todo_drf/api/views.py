368852c4ab631ad92e113abfc78780916d3b067f
unknown <robertkozak97@gmail.com>
2023-02-20 16:48:35 +0100

    CRUD using API

--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -3,7 +3,59 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+# from rest_framework.request import Request
+
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
+
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    api_urls ={
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True) # many=True -> more than one value to serialize
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False) # many=False -> just one item to serialzie
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()   
+
+    return Response("Item succesfully deleted !!!")
